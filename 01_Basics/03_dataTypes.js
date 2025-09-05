3d7ad8eb78c9471925aeae6f4a4baec4b706f3df
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-05 07:07:01 +0000

    datatypes

---
+++ 01_Basics/03_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert(3+3);   // this does not work in the node.js
+// This alert will work in browser console
+
+let age=10;
+let name ="Kaushiki";
+let value = true;
+
+console.log(typeof "kirti");
+console.log(typeof 200);
+console.log(typeof age);
+console.log(typeof null);   // the datatype of null is object, not null itself whereas the datatype of the undefined is undefined (itself)
+console.log(typeof undefined);
