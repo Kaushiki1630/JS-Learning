54e4aaf6dbdb75c868413b0ccfc4d85166ae2449
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-05 13:44:15 +0000

    Conversins and Operations

---
+++ 01_Basics/04_conversionsOperations.js
@@ -0,0 +1,35 @@
+let age = "33abc";
+
+//console.log(typeof age);
+
+let valueInNumber = Number(age);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+// "33" -> 33;
+// "33abc" -> NaN;
+// true -> 1; false -> 0;
+
+let loggedin = "";
+let valueInBoolean = Boolean(loggedin);
+//console.log(typeof valueInBoolean);
+//console.log(valueInBoolean);
+
+// 1=>true; 0=>false;
+//"" => false;
+// "kaush"=> true;
+
+let someNumber = 40;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// ******************************* Operations *******************************
+
+
+
+
+
+
+
+
