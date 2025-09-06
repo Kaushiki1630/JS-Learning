c0eb3c232179f84baa2415930cc864699ec47d89
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-06 00:03:27 +0000

    Operations

--- 01_Basics/04_conversionsOperations.js
+++ 01_Basics/04_conversionsOperations.js
@@ -21,11 +21,45 @@ let valueInBoolean = Boolean(loggedin);
 
 let someNumber = 40;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
 // ******************************* Operations *******************************
 
+let value = 3;
+let negValue = -value;
+
+//console.log(negValue);
+
+// Some Basic Operations:
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello";
+let str2= " kaushiki";
+let str3= str1+str2;
+// console.log(str3);
+
+// below examples ko likhna avoid krna hai because they will confuse you. If you ever want to do such calculations, try to use, brackets !! 
+
+// If you want to learn the idea behind the calculation, refer to ecma 
+// https://tc39.es/ecma262/#sec-abstract-operations
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( 3 + 4 * 5 % 3);
+
+let countOp = 100;
+++countOp;
+console.log(countOp);
 
 
 
