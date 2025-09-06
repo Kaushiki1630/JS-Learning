9da8476c7ab3f2acab671feea5e99bc457b09b0a
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-06 06:20:14 +0000

    Strings

---
+++ 01_Basics/07_strings.js
@@ -0,0 +1,47 @@
+const personName = "Kaushhhhh";
+const repoCount = 2;
+
+// console.log(personName + " " + repoCount + " Value");
+
+console.log(`Hello my name is ${personName} and my repo count is ${repoCount}`);
+
+const gameName = new String('Kaushiki');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toString());
+
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(2,7);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7,7);
+// console.log(anotherString);
+
+const str1 = "      Meow       ";
+console.log(str1);
+console.log(str1.trim());
+
+const sentence = "This is my time to learn JavaScript. JavaScript is an absolutely a awesome lang."
+console.log(sentence.replace('JavaScript', 'React'));
+
+console.log(sentence.replace(/JavaScript/g, 'React'));
+
+const str="JavaScript is Fun";
+console.log(str.split('a'));
+console.log(str.split(" "));
+
+
+
+
+
+
+
+
+
+
+
+
