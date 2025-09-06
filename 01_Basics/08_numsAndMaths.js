0293f95e72688cb7d345ff0232a344ee6e4c0c0f
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-06 07:37:00 +0000

    Numbers and Maths

--- 01_Basics/08_numsAndMaths.js
+++ 01_Basics/08_numsAndMaths.js
@@ -1,1 +1,40 @@
-console.log("Hello World!")
+// console.log("Hello World!");
+
+// const score=100;
+// console.log(score);
+
+// const balance = new Number(400);
+// console.log(balance);
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(4));
+
+// let num1=123.875;
+// console.log(num1.toPrecision(4));
+
+// let num2=12321875;
+// console.log(num2.toLocaleString());
+// console.log(num2.toLocaleString('en-IN'));
+
+// ************ Maths **************
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));   // 4 se thoda sa bhi jada hua toh 5 bna dega
+// console.log(Math.floor(4.2));
+
+console.log(Math.random());    // gives random number between 0 and 1
+
+console.log((Math.random() * 10) + 1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+// formula for generating random number between the given range of min and max
+
+
+
+
