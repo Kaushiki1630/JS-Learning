9135114355c2b073fe92e3f9991b62e4b15e2884
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-07 18:40:37 +0000

    While Loop

---
+++ 05_Iterations/whileLoop.js
@@ -0,0 +1,25 @@
+// while loop
+
+let index=0;
+while (index<=10) {
+   // console.log(`value of index is ${index}`);
+    index = index+2;
+}
+
+let arr=['a','b','c','d','e','f'];
+
+let i=0;
+let n = arr.length;
+while (i<n) {
+   // console.log(`The value at ${i} index in while loop is ${arr[i]}`);
+    i=i+1;
+}
+
+
+// DO WHILE LOOP
+
+let score=11;
+do {
+    console.log(`The score is: ${score}`);
+    score++;
+} while (score<=10);
