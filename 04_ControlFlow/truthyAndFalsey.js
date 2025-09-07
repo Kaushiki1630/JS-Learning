361553a014eeb1f218d96be2bad4d18b2dbe8833
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-07 14:10:57 +0000

    Truthy and Falsy

---
+++ 04_ControlFlow/truthyAndFalsey.js
@@ -0,0 +1,20 @@
+const userEmail = [];
+
+// if(userEmail){
+//     console.log("Got your user email.");
+// }
+// else{
+//     console.log("Do not have user email.");
+// }
+
+// check if the array is empty i.e. truthy 
+
+//check the type is array and also the length is 0
+if(userEmail.length === 0){
+    console.log("Array is empty.");
+}
+
+const obj={};
+if(Object.keys(obj).length === 0){
+    console.log("Object is Array.")
+}
