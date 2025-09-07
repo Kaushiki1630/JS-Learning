0cf6a3c5733dd7743234d09abc4e1915d6eb4375
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-07 04:55:19 +0000

    Scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,43 @@
+var b=300;
+
+if(true){
+    let a=10;
+    var b=20;
+    const c=30;
+}
+
+function one(){
+    const username="hitesh";
+
+    function two(){
+        const website="youtube";
+        console.log(username);
+    }
+
+   // console.log(website);
+    two();
+}
+//one();
+
+// console.log(a);
+//console.log(b);
+// console.log(c);
+
+
+// Same is the working of if else block
+//For eg:
+
+if(true){
+    const username="kaushiki";
+    if(username==="kaushiki"){
+        const website=" youtube";
+       // console.log(username +  website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++ interesting +++++++++++++++++++++
+
+
