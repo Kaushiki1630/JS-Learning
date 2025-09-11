c8c67437769c17a41ae2c59b65e284d8c597c0b7
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-11 06:41:21 +0000

    Unlimited Color Game

---
+++ 08_Events/01_UnlimitedColors/javascript.js
@@ -0,0 +1,27 @@
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+let body = document.querySelector('body');
+
+let timer;
+
+document.querySelector('#start').addEventListener('click', function(){
+    if(!timer){
+        timer = setInterval( function(){
+        body.style.backgroundColor=randomColor();
+    }, 1000);
+    }
+});
+
+document.querySelector('#stop').addEventListener('click', function(){
+    clearInterval(timer);
+    body.backgroundColor=randomColor;
+    timer = null;
+});
