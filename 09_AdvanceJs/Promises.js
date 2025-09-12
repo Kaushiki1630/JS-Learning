4bc22ae5e14a657569809024938f5858128a1002
Kaushiki <kaushikichoudhary3@gmail.com>
2025-09-12 08:27:07 +0000

    Promises

--- 09_AdvanceJs/Promises.js
+++ 09_AdvanceJs/Promises.js
@@ -71,3 +71,51 @@ promiseFour.then((user)=> {
 
 // way 5
 
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username:"JavaScript", password:"1256"});
+        } else{
+            reject('ERROR: Js went wrong');
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response =await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
+
+
+// Example - fetch()
+
+//way 1
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: " , error);
+//     }
+// }
+
+// getAllUsers();
+
+
+// way 2
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) =>{
+    return response.json();
+})
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
